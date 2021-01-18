88cf6af2930326c00f36f63454c9d5b147221f0a
Julio Valsesia <jvalsesia@gmail.com>
2021-01-18 18:10:02 -0300

    Initial project

---
+++ first_demo/src/main/java/com/valsesia/gcp/first_demo/FirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.valsesia.gcp.first_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDemoApplication.class, args);
+	}
+
+}
